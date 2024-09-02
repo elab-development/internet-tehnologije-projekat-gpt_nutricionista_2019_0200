2ca107653b91ce7a6ac8fa2c4bf8a9716fd4eca6
dubravkaD <duca.drcelic25@gmail.com>
2024-09-02 12:23:46 +0200

    kreirane resource klase

---
+++ applaravel/app/Http/Resources/MealPlanResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MealPlanResource extends JsonResource
+{
+    /**
+     * Transformiše resurs u niz.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'start_date' => $this->start_date->format('Y-m-d'),
+            'end_date' => $this->end_date->format('Y-m-d'),
+            'total_calories' => $this->total_calories,
+            'activity_level' => $this->activity_level,
+            'protein_goal' => $this->protein_goal,
+            'fat_goal' => $this->fat_goal,
+            'carb_goal' => $this->carb_goal,
+            'status' => $this->status,
+            'notes' => $this->notes,
+            'user' => new UserResource($this->whenLoaded('user')),  
+            'meals' => MealResource::collection($this->whenLoaded('meals')),  
+        ];
+    }
+}
---
+++ applaravel/app/Http/Resources/MealResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MealResource extends JsonResource
+{
+    /**
+     * Transformiše resurs u niz.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'calories' => $this->calories,
+            'date' => $this->date->format('Y-m-d'),
+            'time' => $this->time->format('H:i'),
+            'ingredients' => $this->ingredients,
+            'instructions' => $this->instructions,
+            'meal_type' => $this->meal_type,
+            'servings' => $this->servings,
+            'meal_plan' => new MealPlanResource($this->whenLoaded('mealPlan')), // Resurs za MealPlan
+        ];
+    }
+}
